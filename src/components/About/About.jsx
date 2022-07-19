7b50d988310a388d72f238934d22055ca8db50a4
Esteban Longo <longoesteban@hotmail.com>
2022-07-19 20:32:21 -0300

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
         {/* Soy una persona proactiva, de mente analítica, con grandes habilidades
         de comunicación y empatía. Con pensamiento creativo, resolución de problemas,
         trabajo en equipo, comunicación y autonomía. Día a día intento mejorar mis habilidades, soy un entusiasta del aprendizaje continuo. */}
-        Mi curiosidad hace de mi día a día mejorar mis habilidades. Soy un entusiasta del continuo aprendizaje. Tanto en lo grupal como en lo individual me reconozco como una persona proactiva, de mente analítica y creativa, con grandes habilidades de comunicación, de resolución de problemas y de trabajo en equipo.
+        Mi curiosidad hace de mi día a día mejorar mis habilidades. Soy un entusiasta del continuo aprendizaje. Tanto en lo grupal como en lo individual me reconozco como una persona proactiva, de mente analítica, resolutiva y creativa, con grandes habilidades de comunicación y de trabajo en equipo.
       </p>
 
       <section id="section05" class="demo">
